fa81ea5143f47f569c2c5fa0d111111bbdfc46dd
EugeneKovtun <eugene.kraros@gmail.com>
2017-07-27 06:34:18 +0300

    Added enums

---
+++ src/main/java/epam/courses/enums/Stems.java
@@ -0,0 +1,8 @@
+package epam.courses.enums;
+
+/**
+ * Created by eugene on 27.07.17.
+ */
+public enum STEMS {
+    ABSENT, SHORT, MIDDLE, LONG
+}
---
+++ src/main/java/epam/courses/enums/Support.java
@@ -0,0 +1,7 @@
+package epam.courses.enums;
+
+/**
+ * Created by eugene on 27.07.17.
+ */
+public class Support {
+}
