c186eb1cdad10e840bdb0729b18d6405f9ad273d
EugeneKovtun <eugene.kraros@gmail.com>
2017-07-27 05:00:52 +0300

    Have been created project with MVC structure

---
+++ src/main/java/epam/courses/App.java
@@ -0,0 +1,14 @@
+package epam.courses;
+
+
+import epam.courses.controller.Controller;
+import epam.courses.model.Model;
+import epam.courses.view.View;
+
+public class App
+{
+    public static void main( String[] args ) {
+        Controller controller = new Controller(new Model(),new View());
+        controller.processUser();
+    }
+}
